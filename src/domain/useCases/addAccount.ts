ad5670378a0048ce1bd1b1298cb0602637bec88b
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-12 19:23:28 -0300

    feat: ensure SignUp Controller calls AddAccount with correct values

---
+++ src/domain/useCases/addAccount.ts
@@ -0,0 +1,7 @@
+import { type AccountModel } from '../models/account';
+
+export interface AddAccountModel extends Omit<AccountModel, 'id'> {}
+
+export interface AddAccount {
+  add: (account: AddAccountModel) => AccountModel;
+}
