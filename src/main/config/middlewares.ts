56afb88aa653fd4637177259cfdd01cf691a0ba0
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-13 20:37:15 -0300

    feat: ensure cors is enabled

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { type Express } from 'express';
 import { bodyParser } from '../middlewares/bodyParser';
+import { cors } from '../middlewares/cors';
 
 export const setupMiddlewares = (app: Express): void => {
   app.use(bodyParser);
+  app.use(cors);
 };
