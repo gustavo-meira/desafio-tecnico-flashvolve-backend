b5908b7154ca80e07cbaa46638104721e153a6f9
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-13 19:38:07 -0300

    refactor: change imports on AccountPrismaRepo to use module

--- src/infra/db/prisma/accountRepo/accountRepo.ts
+++ src/infra/db/prisma/accountRepo/accountRepo.ts
@@ -1,6 +1,6 @@
-import { type AddAccountRepository } from '../../../../data/protocols/addAccountRepository';
-import { type AccountModel } from '../../../../domain/models/account';
-import { type AddAccountModel } from '../../../../domain/useCases/addAccount';
+import { type AddAccountRepository } from '@/data/protocols/addAccountRepository';
+import { type AccountModel } from '@/domain/models/account';
+import { type AddAccountModel } from '@/domain/useCases/addAccount';
 import { prismaDB } from '../lib/db';
 
 export class AccountPrismaRepo implements AddAccountRepository {
