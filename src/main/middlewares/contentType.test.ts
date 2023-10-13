76d6d33a89c86774ad2c909bac4cc5069090a10f
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-13 20:50:55 -0300

    test: ensure requests returns xml when forced

--- src/main/middlewares/contentType.test.ts
+++ src/main/middlewares/contentType.test.ts
@@ -11,4 +11,15 @@ describe('ContentType Middleware', () => {
       .get('/test_content_type')
       .expect('Content-Type', /json/);
   });
+
+  it('Should return xlm content type when forced', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml');
+      res.send();
+    });
+
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('Content-Type', /xml/);
+  });
 });
