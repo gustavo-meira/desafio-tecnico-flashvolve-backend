e53ffb27ec31bb7e5978641f80600f4ac1f293ef
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-14 10:34:09 -0300

    chore: remove Protocols index and the main layer from jest

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,12 @@ import type { Config } from 'jest';
 
 const config: Config = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*Protocols.ts',
+    '!<rootDir>/src/**/index.ts',
+    '!<rootDir>/src/main/**',
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
