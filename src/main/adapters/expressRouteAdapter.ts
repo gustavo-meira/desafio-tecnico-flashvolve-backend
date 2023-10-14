c695b11a47f326730b1e38f3d12d0e7cfad7a65e
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-14 10:34:52 -0300

    feat: create adapt express route

---
+++ src/main/adapters/expressRouteAdapter.ts
@@ -0,0 +1,14 @@
+import { type HttpRequest, type Controller } from '@/presentation/protocols';
+import { type Request, type Response } from 'express';
+
+export const adaptExpressRoute = (controller: Controller) => {
+  return async (req: Request, res: Response) => {
+    const httpRequest: HttpRequest = {
+      body: req.body,
+    };
+
+    const { statusCode, body } = await controller.handle(httpRequest);
+
+    res.status(statusCode).json(body);
+  };
+};
--- src/main/routes/signup.routes.ts
+++ src/main/routes/signup.routes.ts
@@ -1,7 +1,7 @@
 import { type Router } from 'express';
+import { makeSignUpController } from '../factories/signup';
+import { adaptExpressRoute } from '../adapters/expressRouteAdapter';
 
 export const signupRoutes = (router: Router): void => {
-  router.post('/signup', (req, res) => {
-    res.status(201).send();
-  });
+  router.post('/signup', adaptExpressRoute(makeSignUpController()));
 };
