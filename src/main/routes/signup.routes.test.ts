df2cd28fb3949706a0825ef12957f1374c757036
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-14 10:35:33 -0300

    refactor: mock the database on Sign Up Routes test

--- src/main/routes/signup.routes.test.ts
+++ src/main/routes/signup.routes.test.ts
@@ -1,6 +1,23 @@
 import request from 'supertest';
 import { app } from '../config/app';
 
+jest.mock('../../infra/db/prisma/lib/db', () => ({
+  prismaDB: {
+    user: {
+      async create () {
+        await new Promise((resolve) => setTimeout(resolve, 1));
+
+        return {
+          id: 'any_id',
+          name: 'any_name',
+          email: 'any_email@email.com',
+          password: 'any_password',
+        };
+      },
+    },
+  },
+}));
+
 describe('SignUp Routes', () => {
   it('Should return a 201 status code on success', async () => {
     await request(app)
