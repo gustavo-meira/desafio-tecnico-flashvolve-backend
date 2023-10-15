afc50c3ded4bd22301e632f4e5cbc89988ed4153
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-15 19:32:52 -0300

    test: ensure RequiredFieldValidation return falsy if validation succeeds

--- src/presentation/validators/requiredFieldValidation.spec.ts
+++ src/presentation/validators/requiredFieldValidation.spec.ts
@@ -4,8 +4,10 @@ import Chance from 'chance';
 
 const chance = new Chance();
 
+const fieldName = chance.word();
+
 const makeSut = (): RequiredFieldValidation => {
-  const sut = new RequiredFieldValidation('field');
+  const sut = new RequiredFieldValidation(fieldName);
 
   return sut;
 };
@@ -13,10 +15,19 @@ const makeSut = (): RequiredFieldValidation => {
 describe('RequiredField Validation', () => {
   it('Should return a missing param error if validation fails', () => {
     const sut = makeSut();
+
     const error = sut.validate({
       name: chance.name(),
     });
+    expect(error).toEqual(new MissingParamError(fieldName));
+  });
 
-    expect(error).toEqual(new MissingParamError('field'));
+  it('Should return falsy if validation succeeds', () => {
+    const sut = makeSut();
+
+    const error = sut.validate({
+      [fieldName]: chance.name(),
+    });
+    expect(error).toBeFalsy();
   });
 });
