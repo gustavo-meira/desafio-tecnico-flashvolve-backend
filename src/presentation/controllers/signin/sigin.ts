40cbc76511f996017e3a5d9870c824b3d256b80b
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-15 17:53:33 -0300

    feat: ensure SignIn Controller calls Authentication with correct values

--- src/presentation/controllers/signin/sigin.ts
+++ src/presentation/controllers/signin/sigin.ts
@@ -4,11 +4,15 @@ import {
   type HttpResponse,
   type Controller,
   type EmailValidator,
+  type Authentication,
 } from './siginProtocols';
 import { badRequest, serverError } from '@/presentation/helpers/httpHelpers';
 
 export class SignInController implements Controller {
-  constructor (private readonly emailValidator: EmailValidator) {}
+  constructor (
+    private readonly emailValidator: EmailValidator,
+    private readonly authentication: Authentication,
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
@@ -20,13 +24,13 @@ export class SignInController implements Controller {
         }
       }
 
-      const { email } = httpRequest.body;
-
-      const isEmailValid = this.emailValidator.isValid(email);
+      const isEmailValid = this.emailValidator.isValid(httpRequest.body.email);
 
       if (!isEmailValid) {
         return badRequest(new InvalidParamError('email'));
       }
+
+      await this.authentication.auth(httpRequest.body);
     } catch (error) {
       return serverError();
     }
--- src/presentation/controllers/signin/siginProtocols.ts
+++ src/presentation/controllers/signin/siginProtocols.ts
@@ -1,2 +1,3 @@
 export * from '@/presentation/protocols/index';
 export * from '@/presentation/protocols/emailValidator';
+export * from '@/domain/useCases/authentication';
