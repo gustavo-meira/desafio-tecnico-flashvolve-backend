71715ca5f6f7924a0d0f137a4037ae5a6d2c3f6e
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-15 19:29:35 -0300

    feat: ensure RequiredFieldValidation return a MissingParamError if validation fails

---
+++ src/presentation/validators/requiredFieldValidation.ts
@@ -0,0 +1,12 @@
+import { MissingParamError } from '../errors';
+import { type InputValidation, type Validation } from './validation';
+
+export class RequiredFieldValidation implements Validation {
+  constructor (private readonly fieldName: string) {}
+
+  validate (input: InputValidation): Error | null {
+    if (!input[this.fieldName]) {
+      return new MissingParamError(this.fieldName);
+    }
+  }
+}
