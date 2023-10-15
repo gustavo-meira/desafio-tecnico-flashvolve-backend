6b06331d42efb791ea7ab9710a63d2e431aa798e
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-15 19:21:53 -0300

    feat: ensure ValidationComposite returns an error if any validation fails

---
+++ src/presentation/validators/validationComposite.ts
@@ -0,0 +1,15 @@
+import { type InputValidation, type Validation } from './validation';
+
+export class ValidationComposite implements Validation {
+  constructor (private readonly validations: Validation[]) {}
+
+  validate (input: InputValidation): Error | null {
+    for (const validations of this.validations) {
+      const error = validations.validate(input);
+
+      if (error) {
+        return error;
+      }
+    }
+  }
+}
