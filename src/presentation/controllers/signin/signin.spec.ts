813d00753ffd6519ab577023cc46c4068fed07f2
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-15 17:19:58 -0300

    test: ensure SignIn Controller returns 400 if no email is provided

---
+++ src/presentation/controllers/signin/signin.spec.ts
@@ -0,0 +1,24 @@
+import { MissingParamError } from '@/presentation/errors';
+import { SignInController } from './sigin';
+import Chance from 'chance';
+import { badRequest } from '@/presentation/helpers/httpHelpers';
+
+const chance = new Chance();
+
+const signInAccount = {
+  email: chance.email(),
+  password: chance.string(),
+};
+
+describe('SignIn Controller', () => {
+  it('Should return 400 if no email is provided', async () => {
+    const sut = new SignInController();
+
+    const httpResponse = await sut.handle({
+      body: {
+        password: signInAccount.password,
+      },
+    });
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('email')));
+  });
+});
