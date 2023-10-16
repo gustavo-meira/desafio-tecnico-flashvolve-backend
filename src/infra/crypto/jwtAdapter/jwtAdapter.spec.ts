875d75dd1fb4f5e607af1c62807951a45733079b
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-16 13:10:31 -0300

    test: ensure JwtAdapter calls sign with correct values

--- src/infra/crypto/jwtAdapter/jwtAdapter.spec.ts
+++ src/infra/crypto/jwtAdapter/jwtAdapter.spec.ts
@@ -1,5 +1,25 @@
-describe('Jwt Adapter', () => {
-  it('Should', () => {
+import { JwtAdapter } from './jwtAdapter';
+import jwt from 'jsonwebtoken';
+import Chance from 'chance';
 
+const chance = new Chance();
+
+const signedValue = chance.string();
+const jwtSecret = chance.string();
+
+jest.mock('jsonwebtoken', () => ({
+  sign: async (): Promise<string> => {
+    return await Promise.resolve(signedValue);
+  },
+}));
+
+describe('Jwt Adapter', () => {
+  it('Should call sign with correct values', () => {
+    const sut = new JwtAdapter(jwtSecret);
+    const signSpy = jest.spyOn(jwt, 'sign');
+    const valueToSign = chance.string();
+
+    sut.generate(valueToSign);
+    expect(signSpy).toHaveBeenCalledWith({ id: valueToSign }, jwtSecret);
   });
 });
