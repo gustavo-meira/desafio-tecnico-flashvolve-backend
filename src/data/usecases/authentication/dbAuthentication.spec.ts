f73786b5476e3104ea81213518b5e26177235975
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-15 23:34:01 -0300

    test: ensure DBAuthentication calls LoadAccountByEmailRepo with correct email

---
+++ src/data/usecases/authentication/dbAuthentication.spec.ts
@@ -0,0 +1,52 @@
+import { type AccountModel } from '@/domain/models/account';
+import { DbAuthentication } from './dbAuthentication';
+import Chance from 'chance';
+import { type LoadAccountByEmailRepository } from '@/data/protocols/loadAccountByEmailRepository';
+
+const chance = new Chance();
+
+const accountToFind = {
+  email: chance.email(),
+  password: chance.string(),
+};
+
+const accountToReturn: AccountModel = {
+  id: chance.guid(),
+  name: chance.name(),
+  ...accountToFind,
+};
+
+const makeLoadAccountByEmailRepo = (): LoadAccountByEmailRepository => {
+  class LoadAccountByEmailRepoStub implements LoadAccountByEmailRepository {
+    async load (email: string): Promise<AccountModel> {
+      return await new Promise(resolve => { resolve(accountToReturn); });
+    }
+  }
+
+  return new LoadAccountByEmailRepoStub();
+};
+
+interface SutType {
+  sut: DbAuthentication;
+  loadAccountByEmailRepo: LoadAccountByEmailRepository;
+};
+
+const makeSut = (): SutType => {
+  const loadAccountByEmailRepo = makeLoadAccountByEmailRepo();
+  const sut = new DbAuthentication(loadAccountByEmailRepo);
+
+  return {
+    sut,
+    loadAccountByEmailRepo,
+  };
+};
+
+describe('DBAuthentication UseCase', () => {
+  it('Should call LoadAccountByEmailRepo with correct email', async () => {
+    const { sut, loadAccountByEmailRepo } = makeSut();
+    const loadSpy = jest.spyOn(loadAccountByEmailRepo, 'load');
+
+    await sut.auth(accountToFind);
+    expect(loadSpy).toHaveBeenCalledWith(accountToFind.email);
+  });
+});
