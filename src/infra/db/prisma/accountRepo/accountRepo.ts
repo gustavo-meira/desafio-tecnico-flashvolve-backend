ef44b0b60b84caf8920ec5dcfa706bb67c68b535
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-16 15:12:05 -0300

    feat: ensure AccountPrismaRepository return an account on loadByEmail success

--- src/infra/db/prisma/accountRepo/accountRepo.ts
+++ src/infra/db/prisma/accountRepo/accountRepo.ts
@@ -2,11 +2,22 @@ import { type AddAccountRepository } from '@/data/protocols/addAccountRepository
 import { type AccountModel } from '@/domain/models/account';
 import { type AddAccountModel } from '@/domain/useCases/addAccount';
 import { prismaDB } from '../lib/db';
+import { type LoadAccountByEmailRepository } from '@/data/protocols/loadAccountByEmailRepository';
 
-export class AccountPrismaRepo implements AddAccountRepository {
+export class AccountPrismaRepo implements AddAccountRepository, LoadAccountByEmailRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const account = await prismaDB.user.create({
       data: accountData,
+    });
+
+    return account;
+  }
+
+  async loadByEmail (email: string): Promise<AccountModel> {
+    const account = await prismaDB.user.findUnique({
+      where: {
+        email,
+      },
     });
 
     return account;
