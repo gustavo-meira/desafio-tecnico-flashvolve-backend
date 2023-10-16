66ae0033acdbebd17cd66bb2d0e28b15950f5a9c
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-15 22:00:45 -0300

    refactor: export ValidationBuilder on index of validators

--- src/presentation/validators/index.ts
+++ src/presentation/validators/index.ts
@@ -3,3 +3,4 @@ export * from './compareFieldsValidation/compareFieldsValidation';
 export * from './emailValidation/emailValidation';
 export * from './requiredFieldValidation/requiredFieldValidation';
 export * from './validationComposite/validationComposite';
+export * from './builder/validationBuilder';
