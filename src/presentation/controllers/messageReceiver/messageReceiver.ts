fb70dc58fbae46426df6c86c8037d8ed5e39fc03
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-17 12:28:26 -0300

    feat: ensure MessageReceiver Controller returns 500 if AddMessageRepository throws

--- src/presentation/controllers/messageReceiver/messageReceiver.ts
+++ src/presentation/controllers/messageReceiver/messageReceiver.ts
@@ -1,12 +1,17 @@
 import { type AddMessageRepository } from '@/data/protocols/addMessageRepository';
+import { serverError } from '@/presentation/helpers/httpHelpers';
 import { type Controller, type HttpRequest, type HttpResponse } from '@/presentation/protocols';
 
 export class MessageReceiverController implements Controller {
   constructor (private readonly addMessageRepository: AddMessageRepository) { }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.addMessageRepository.add(httpRequest.body);
+    try {
+      await this.addMessageRepository.add(httpRequest.body);
 
-    return null;
+      return null;
+    } catch (error) {
+      return serverError();
+    }
   }
 }
