aaf7e52aba2894c564fdcc1b9349eabef128cc3b
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-17 16:51:18 -0300

    test: ensure MessagePrisma Repo calls prisma with correct values

---
+++ src/infra/db/prisma/messageRepo/messageRepo.spec.ts
@@ -0,0 +1,66 @@
+import { type AddMessageModel } from '@/domain/useCases/addMessage';
+import { MessagePrismaRepo } from './messageRepo';
+import { type MessageModel } from '@/domain/models/message';
+import { prismaDB } from '../lib/db';
+import Chance from 'chance';
+
+const chance = new Chance();
+
+const messageData: AddMessageModel = {
+  text: chance.string(),
+  senderName: chance.name(),
+  fromBot: chance.bool(),
+  chatId: chance.integer({ min: 1000, max: 9999 }),
+};
+
+const messageToResponse: MessageModel = {
+  ...messageData,
+  id: chance.integer({ min: 1000, max: 9999 }),
+  createdAt: chance.date(),
+  updatedAt: chance.date(),
+};
+
+jest.mock('../lib/db', () => ({
+  prismaDB: {
+    message: {
+      create: async () => {
+        await new Promise((resolve) => setTimeout(resolve, 1));
+
+        return messageToResponse;
+      },
+    },
+  },
+}));
+
+const makeSut = (): MessagePrismaRepo => {
+  const sut = new MessagePrismaRepo();
+
+  return sut;
+};
+
+describe('MessagePrisma Repo', () => {
+  it('Should call prisma with correct values', async () => {
+    const sut = makeSut();
+    const createSpy = jest.spyOn(prismaDB.message, 'create');
+
+    await sut.add(messageData);
+    expect(createSpy).toHaveBeenCalledWith({
+      data: {
+        senderName: messageData.senderName,
+        text: messageData.text,
+        fromBot: messageData.fromBot,
+        Chat: {
+          connectOrCreate: {
+            where: {
+              id: messageData.chatId,
+            },
+            create: {
+              lastMessage: messageData.text,
+              name: messageData.senderName,
+            },
+          },
+        },
+      },
+    });
+  });
+});
