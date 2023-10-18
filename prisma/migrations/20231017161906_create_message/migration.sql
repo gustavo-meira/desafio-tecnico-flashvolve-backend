b1620b26e573017a008bd57a6fe43c97f7a7c2ab
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-18 15:01:30 -0300

    chore: modify prisma schema to id in chat and messages be a bigint

--- prisma/migrations/20231017161906_create_message/migration.sql
+++ prisma/migrations/20231017161906_create_message/migration.sql
@@ -1,23 +1,23 @@
 -- CreateTable
 CREATE TABLE "Chat" (
-    "id" SERIAL NOT NULL,
+    "id" BIGSERIAL NOT NULL,
+    "name" TEXT NOT NULL,
     "lastMessage" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "name" TEXT NOT NULL,
 
     CONSTRAINT "Chat_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Message" (
-    "id" SERIAL NOT NULL,
+    "id" BIGSERIAL NOT NULL,
     "text" TEXT NOT NULL,
     "senderName" TEXT NOT NULL,
     "fromBot" BOOLEAN NOT NULL DEFAULT false,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "chatId" INTEGER NOT NULL,
+    "chatId" BIGINT NOT NULL,
 
     CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
 );
