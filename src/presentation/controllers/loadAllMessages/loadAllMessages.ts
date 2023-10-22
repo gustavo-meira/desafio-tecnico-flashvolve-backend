fa9c8f2c6d79d806bb8e83f26b297761c798039f
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-22 13:31:46 -0300

    feat: ensure LoadAllMessages Controller return badRequest if Validation fails

--- src/presentation/controllers/loadAllMessages/loadAllMessages.ts
+++ src/presentation/controllers/loadAllMessages/loadAllMessages.ts
@@ -1,10 +1,15 @@
+import { badRequest } from '@/presentation/helpers/httpHelpers';
 import { type Validation, type Controller, type HttpRequest, type HttpResponse } from '../signup/signupProtocols';
 
 export class LoadAllMessagesController implements Controller {
   constructor (private readonly validation: Validation) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    this.validation.validate(httpRequest.body);
+    const error = this.validation.validate(httpRequest.body);
+
+    if (error) {
+      return badRequest(error);
+    }
 
     return null;
   }
