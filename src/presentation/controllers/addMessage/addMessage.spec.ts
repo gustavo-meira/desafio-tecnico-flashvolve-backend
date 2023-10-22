91907f01d54cad90069e872c01327839b7fa373e
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-22 17:55:43 -0300

    test: ensure AddMessage Controller call Validation with correct values

---
+++ src/presentation/controllers/addMessage/addMessage.spec.ts
@@ -0,0 +1,48 @@
+import { type HttpRequest, type InputValidation, type Validation } from '../signup/signupProtocols';
+import { AddMessageController } from './addMessage';
+import Chance from 'chance';
+
+const chance = new Chance();
+
+const httpRequest: HttpRequest = {
+  body: {
+    chatId: chance.integer(),
+    text: chance.sentence(),
+    senderName: chance.name(),
+  },
+};
+
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: InputValidation): Error | null {
+      return null;
+    }
+  }
+
+  return new ValidationStub();
+};
+
+interface SutTypes {
+  sut: AddMessageController;
+  validationStub: Validation;
+}
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidation();
+  const sut = new AddMessageController(validationStub);
+
+  return {
+    sut,
+    validationStub,
+  };
+};
+
+describe('AddMessage Controller', () => {
+  it('Should call Validation with correct values', async () => {
+    const { sut, validationStub } = makeSut();
+    const validateSpy = jest.spyOn(validationStub, 'validate');
+
+    await sut.handle(httpRequest);
+    expect(validateSpy).toHaveBeenCalledWith(httpRequest.body);
+  });
+});
