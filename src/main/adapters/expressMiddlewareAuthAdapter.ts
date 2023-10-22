4f489b5ad3cb8f6c469b176cb9b4ec17f7304df7
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-22 04:33:20 -0300

    refactor: change adaptExpressAuthMiddleware to use authorization header

--- src/main/adapters/expressMiddlewareAuthAdapter.ts
+++ src/main/adapters/expressMiddlewareAuthAdapter.ts
@@ -3,7 +3,11 @@ import { type NextFunction, type Request, type Response } from 'express';
 
 export const adaptExpressAuthMiddleware = (middleware: Middleware<any>) => {
   return async (req: Request, res: Response, next: NextFunction) => {
-    const { statusCode, body } = await middleware.handle(req.body);
+    const httpRequest = {
+      accessToken: req.headers?.authorization?.split(' ')[1],
+    };
+
+    const { statusCode, body } = await middleware.handle(httpRequest);
 
     if (statusCode !== 200) {
       return res.status(statusCode).json(body);
