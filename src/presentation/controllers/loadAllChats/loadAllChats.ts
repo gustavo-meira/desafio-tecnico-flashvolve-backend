413bc06cae59aecfcaa00060f09038d0b2cc80fe
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-22 03:41:14 -0300

    feat: ensure LoadAllChats Controller return 500 if LoadAllChats throws

--- src/presentation/controllers/loadAllChats/loadAllChats.ts
+++ src/presentation/controllers/loadAllChats/loadAllChats.ts
@@ -1,12 +1,17 @@
 import { type LoadAllChats } from '@/domain/useCases/loadAllChats';
+import { serverError } from '@/presentation/helpers/httpHelpers';
 import { type Controller, type HttpRequest, type HttpResponse } from '@/presentation/protocols';
 
 export class LoadAllChatsController implements Controller {
   constructor (private readonly loadAllChats: LoadAllChats) {}
 
   async handle (request: HttpRequest): Promise<HttpResponse> {
-    await this.loadAllChats.loadAll();
+    try {
+      await this.loadAllChats.loadAll();
 
-    return null;
+      return null;
+    } catch (err) {
+      return serverError();
+    }
   }
 }
