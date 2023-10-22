53f22619b0d0cdf46bef9cf5380197ad7d2359b0
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-22 15:41:01 -0300

    refactor: adaptExpressRoute now uses req.params as httpRequest body

--- src/main/adapters/expressRouteAdapter.ts
+++ src/main/adapters/expressRouteAdapter.ts
@@ -4,7 +4,10 @@ import { type Request, type Response } from 'express';
 export const adaptExpressRoute = (controller: Controller) => {
   return async (req: Request, res: Response) => {
     const httpRequest: HttpRequest = {
-      body: req.body,
+      body: {
+        ...(req.body || {}),
+        ...(req.params || {}),
+      },
     };
 
     const { statusCode, body } = await controller.handle(httpRequest);
