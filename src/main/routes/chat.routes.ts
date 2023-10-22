c6c0921fc0e166345548d08dc8d54b56847e6bc5
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-22 16:30:16 -0300

    feat: create route GET '/api/chat/:chatId/messages' to get all messages

--- src/main/routes/chat.routes.ts
+++ src/main/routes/chat.routes.ts
@@ -3,7 +3,18 @@ import { authMiddleware } from '../factories/authMiddleware';
 import { adaptExpressAuthMiddleware } from '../adapters/expressMiddlewareAuthAdapter';
 import { adaptExpressRoute } from '../adapters/expressRouteAdapter';
 import { makeLoadAllChatsController } from '../factories/loadAllChats';
+import { makeLoadAllMessagesController } from '../factories/loadAllMessages';
 
 export const chatRoutes = (router: Router): void => {
-  router.get('/chat', adaptExpressAuthMiddleware(authMiddleware), adaptExpressRoute(makeLoadAllChatsController()));
+  router.get(
+    '/chat',
+    adaptExpressAuthMiddleware(authMiddleware),
+    adaptExpressRoute(makeLoadAllChatsController()),
+  );
+
+  router.get(
+    '/chat/:chatId/messages',
+    adaptExpressAuthMiddleware(authMiddleware),
+    adaptExpressRoute(makeLoadAllMessagesController()),
+  );
 };
