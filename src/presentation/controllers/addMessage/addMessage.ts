1c5e48c445bc366a045f08f86b2a1218de27c3fe
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-22 18:04:22 -0300

    feat: ensure AddMessage Controller call AddMessage with correct values

--- src/presentation/controllers/addMessage/addMessage.ts
+++ src/presentation/controllers/addMessage/addMessage.ts
@@ -1,8 +1,12 @@
 import { badRequest } from '@/presentation/helpers/httpHelpers';
 import { type HttpRequest, type Controller, type HttpResponse, type Validation } from '../signup/signupProtocols';
+import { type AddMessage } from '@/domain/useCases/addMessage';
 
 export class AddMessageController implements Controller {
-  constructor (private readonly validation: Validation) {}
+  constructor (
+    private readonly validation: Validation,
+    private readonly addMessage: AddMessage,
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const error = this.validation.validate(httpRequest.body);
@@ -10,6 +14,11 @@ export class AddMessageController implements Controller {
     if (error) {
       return badRequest(error);
     }
+
+    await this.addMessage.add({
+      ...httpRequest.body,
+      fromBot: true,
+    });
 
     return null;
   }
