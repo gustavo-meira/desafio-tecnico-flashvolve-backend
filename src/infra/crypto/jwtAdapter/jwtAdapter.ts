f9774bbea7458df28b9603fcbc1d1e3ecb74e668
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-22 00:19:53 -0300

    feat: ensure Jwt Adapter calls verify with correct values

--- src/infra/crypto/jwtAdapter/jwtAdapter.ts
+++ src/infra/crypto/jwtAdapter/jwtAdapter.ts
@@ -1,7 +1,8 @@
 import jwt from 'jsonwebtoken';
 import { type TokenGenerator } from '@/data/protocols/tokenGenerator';
+import { type TokenDecrypter } from '@/data/protocols/tokenDecrypter';
 
-export class JwtAdapter implements TokenGenerator {
+export class JwtAdapter implements TokenGenerator, TokenDecrypter {
   constructor (private readonly secret: string) {}
 
   generate (id: string): string {
@@ -9,4 +10,10 @@ export class JwtAdapter implements TokenGenerator {
 
     return token;
   }
+
+  async decrypt (value: string): Promise<string> {
+    jwt.verify(value, this.secret);
+
+    return null;
+  };
 }
