e6f8c2ac951820a9d0f690c87ee3b4b093629f34
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-22 13:41:49 -0300

    feat: ensure LoadAllMessages Controller call LoadAllMessages with correct values

--- src/presentation/controllers/loadAllMessages/loadAllMessages.ts
+++ src/presentation/controllers/loadAllMessages/loadAllMessages.ts
@@ -1,8 +1,12 @@
 import { badRequest } from '@/presentation/helpers/httpHelpers';
 import { type Validation, type Controller, type HttpRequest, type HttpResponse } from '../signup/signupProtocols';
+import { type LoadMessages } from '@/domain/useCases/loadMessages';
 
 export class LoadAllMessagesController implements Controller {
-  constructor (private readonly validation: Validation) {}
+  constructor (
+    private readonly validation: Validation,
+    private readonly loadMessages: LoadMessages,
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const error = this.validation.validate(httpRequest.body);
@@ -10,6 +14,8 @@ export class LoadAllMessagesController implements Controller {
     if (error) {
       return badRequest(error);
     }
+
+    await this.loadMessages.load(httpRequest.body.chatId);
 
     return null;
   }
