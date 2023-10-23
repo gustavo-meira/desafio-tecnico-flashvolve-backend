91053932b1be7859ba52281e9e92c5de1b160989
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-23 00:01:37 -0300

    refactor: AddAccount should return string on success

--- src/domain/useCases/addAccount.ts
+++ src/domain/useCases/addAccount.ts
@@ -3,5 +3,5 @@ import { type AccountModel } from '@/domain/models/account';
 export interface AddAccountModel extends Omit<AccountModel, 'id'> {}
 
 export interface AddAccount {
-  add: (account: AddAccountModel) => Promise<AccountModel>;
+  add: (account: AddAccountModel) => Promise<string>;
 }
