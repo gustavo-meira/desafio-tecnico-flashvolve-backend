a02e5f045719668fc4fa406bc449e544f40d0d1f
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-23 11:05:18 -0300

    test: Axios Adapter call axios with correct url and body

---
+++ src/infra/http/axiosAdapter.spec.ts
@@ -0,0 +1,46 @@
+import { type MessageModel } from '@/domain/models/message';
+import { AxiosAdapter } from './axiosAdapter';
+import axios from 'axios';
+import Chance from 'chance';
+
+const chance = new Chance();
+
+const url = chance.url();
+const botToken = chance.string({ length: 12 });
+
+const urlToPost = `${url}/bot${botToken}/sendMessage`;
+
+const messageToSend: MessageModel = {
+  id: chance.integer(),
+  chatId: chance.integer(),
+  fromBot: chance.bool(),
+  senderName: chance.name(),
+  text: chance.string(),
+  createdAt: chance.date(),
+  updatedAt: chance.date(),
+};
+
+jest.mock('axios', () => ({
+  async post (): Promise<void> {
+    await Promise.resolve();
+  },
+}));
+
+const makeSut = (): AxiosAdapter => {
+  const sut = new AxiosAdapter(url, botToken);
+
+  return sut;
+};
+
+describe('Axios Adapter', () => {
+  it('Should call axios with correct url and body', async () => {
+    const sut = makeSut();
+    const postSpy = jest.spyOn(axios, 'post');
+
+    await sut.postMessage(messageToSend);
+    expect(postSpy).toHaveBeenCalledWith(urlToPost, {
+      chat_id: messageToSend.chatId,
+      text: messageToSend.text,
+    });
+  });
+});
